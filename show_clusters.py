4e4ff5121823a5200d416284f083614bda7e254f
MarioMarkov <mario11septemvri@gmail.com>
2023-06-07 18:22:47 +0200

    script to show cluster images

--- show_clusters.py
+++ show_clusters.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71b28b88c3a6d7229950060c1bf21ecd1611b18cde1991749b774e916f4d9b3f
-size 5262
+oid sha256:a19d73dc5efcba3e10f3c7e7c8ad9c00c9ff093d11e6c5ef60647b87f3cbef18
+size 5234
